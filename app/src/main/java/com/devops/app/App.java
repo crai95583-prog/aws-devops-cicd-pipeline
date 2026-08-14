4a09ed93c3f91105b3b4ca4140d60b4c38195514
Chandan Rai <crai95583-prog>
2026-08-14 08:22:30 +0000

    Add Java Maven application

---
+++ app/src/main/java/com/devops/app/App.java
@@ -0,0 +1,9 @@
+package com.devops.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("AWS DevOps CI/CD Pipeline Application");
+        System.out.println("Application started successfully!");
+    }
+}
